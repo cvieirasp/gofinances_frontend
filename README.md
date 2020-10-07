7aa1998f9793e8e6c2a1feab5fad685cade9a01e
Carlos Figueiredo <cvieirasp@gmail.com>
2020-10-07 15:12:29 -0300

    Primeira versão da aplicação

---
+++ README.md
@@ -0,0 +1,5 @@
+## GoFinances Frontend
+
+Frontend **GoFinances**
+
+*Aplicação para armazenar transações financeiras de entrada e saída, que deve permitir o cadastro e a listagem dessas transações.*
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -12,6 +12,11 @@ import formatValue from '../../utils/formatValue';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
+interface ResponseData {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 interface Transaction {
   id: string;
   title: string;
@@ -30,12 +35,21 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance | null>(null);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get<ResponseData>('transactions');
+      const result = response.data.transactions;
+      result.forEach((trans, index) => {
+        result[index].formattedValue = formatValue(trans.value);
+        result[index].formattedDate = new Date(
+          trans.created_at,
+        ).toLocaleDateString('pt-BR');
+      });
+      setTransactions(result);
+      setBalance(response.data.balance);
     }
 
     loadTransactions();
@@ -51,21 +65,27 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">
+              {balance && formatValue(Number(balance.income))}
+            </h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">
+              {balance && `- ${formatValue(Number(balance.outcome))}`}
+            </h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">
+              {balance && formatValue(Number(balance.total))}
+            </h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +101,14 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(trans => (
+                <tr key={trans.id}>
+                  <td className="title">{trans.title}</td>
+                  <td className="income">{trans.formattedValue}</td>
+                  <td>{trans.category.title}</td>
+                  <td>{trans.formattedDate}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
